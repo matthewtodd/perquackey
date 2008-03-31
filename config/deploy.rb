05c763385c63b2df260f4057ed88fad88a631c2c
Matthew Todd <matthew.todd@gmail.com>
2008-03-31 17:58:31 +0300

    Fixed start/stop/restart to play nicely with Solaris Bootup Actions.

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,3 +18,29 @@ role :db,  'woodward.joyent.us', :user => 'matthew', :primary => true
 set :runner, 'matthew'
 set :run_method, :run
 set :group_writable, false
+
+# Joyent recommend starting and monitoring Rails processes as "Bootup Actions".
+# From my experimentation, it seems like trying to spawn Mongrel via Capistrano
+# in the usual way interferes with (and is interfered with by) these Bootup
+# Actions. So, I'm disabling start and overriding restart to just do a "stop".
+# The Solaris Bootup Action will notice when Mongrel dies and restart it 
+# immediately.
+namespace :deploy do
+  desc 'Start the application servers.'
+  task :start do
+    puts 'To start the application, please use the Bootup Actions at'
+    puts 'https://virtualmin.joyent.us/woodward/virtualmin-init/index.cgi?dom=120695737911739'
+  end
+  
+  desc 'Restarts your application.'
+  task :restart do
+    stop
+  end
+  
+  desc 'Stop the application servers.'
+  task :stop, :roles => :app do
+    as = fetch(:runner, "app")
+    via = fetch(:run_method, :sudo)
+    invoke_command "#{current_path}/script/process/reaper -a kill", :via => via, :as => as
+  end
+end

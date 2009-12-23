4926e085ed239e872b3b787f5811b589ad9aa2ec
Matthew Todd <matthew.todd@gmail.com>
2009-12-23 13:09:19 -0500

    Externalize sinatra templates.

--- lib/perquackey/server.rb
+++ lib/perquackey/server.rb
@@ -3,6 +3,8 @@ require 'sinatra/base'
 
 module Perquackey
   class Server < Sinatra::Base
+    set :views, File.dirname(__FILE__) + '/server/views'
+
     get '/' do
       @letters = ''
       @table   = Perquackey::WordTable.new
@@ -18,49 +20,6 @@ module Perquackey
       @letters = params[:letters]
       @table   = Perquackey::Game.new.words(@letters)
       erb :letters
-    end
-
-    template :layout do
-      layout = <<-END_HTML.strip
-    <?xml version="1.0" encoding="utf-8" ?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
-      <head>
-        <title><%= "Perquackey: #{@letters}" %></title>
-      </head>
-      <body>
-        <h1>Perquackey</h1>
-        <form name="form" action="/" method="post">
-          <input name="letters" value="<%= @letters %>">
-        </form>
-        <script type="text/javascript">
-          document.forms.form.letters.focus();
-        </script>
-        <%= yield %>
-        <p>Thanks to Mendel Cooper and Alan Beale for the <a href="http://personal.riverusers.com/~thegrendel/software.html">YAWL Word List</a>.</p>
-      </body>
-    </html>
-      END_HTML
-    end
-
-    template :letters do
-      letters = <<-END_HTML.strip
-    <table>
-      <tr>
-        <% @table.headers.each do |header| %>
-          <th><%= header %></th>
-        <% end %>
-      </tr>
-
-      <% @table.each do |row| %>
-        <tr>
-          <% row.each do |word| %>
-            <td><%= word %></td>
-          <% end %>
-        </tr>
-      <% end %>
-    </table>
-      END_HTML
     end
   end
 end

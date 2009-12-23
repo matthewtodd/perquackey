7ea75b49f0203f06637d1eb41c02ae42a0ec2b85
Matthew Todd <matthew.todd@gmail.com>
2009-12-23 13:22:16 -0500

    Remove unnecessary intermediate directory.

--- lib/perquackey/server.rb
+++ lib/perquackey/server.rb
@@ -3,7 +3,7 @@ require 'sinatra/base'
 
 module Perquackey
   class Server < Sinatra::Base
-    set :views, File.dirname(__FILE__) + '/server/views'
+    set :views, File.dirname(__FILE__) + '/server'
 
     get %r{^/([a-z]{0,13})$} do |letters|
       erb :letters, :locals => { :letters => letters, :table => Game.new.words(letters) }

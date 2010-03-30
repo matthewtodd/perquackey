f41e456824e33bd9d209c9e52ed829bc732b258c
Matthew Todd <matthew.todd@gmail.com>
2010-03-30 13:50:42 +0300

    Include TWL word list.

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class DictionaryTest < Test::Unit::TestCase
-  def test_simple_search
+  def test_simple_search_with_twl
+    assert_equal ['ab', 'ba', 'cab'], Perquackey::Dictionary.new(Perquackey::Game::TWL).words('abc')
+  end
+
+  def test_simple_search_with_yawl
     assert_equal ['ab', 'ba', 'cab'], Perquackey::Dictionary.new(Perquackey::Game::YAWL).words('abc')
   end
 end

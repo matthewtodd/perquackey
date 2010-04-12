dc3bfa5c9a3d97a9a70b8dd5a722a4942c2ce292
Matthew Todd <matthew.todd@gmail.com>
2010-04-12 15:41:03 +0300

    Futz around with Perquackey::Application.

--- lib/perquackey/application.rb
+++ lib/perquackey/application.rb
@@ -3,25 +3,27 @@ require 'ostruct'
 
 module Perquackey
   class Application
-    def run!(*args)
-      conf = OpenStruct.new(:mode => :console, :port => 3000)
+    def initialize(argv)
+      @conf = OpenStruct.new(:mode => :console, :port => 3000)
 
       opts = OptionParser.new do |opts|
         opts.banner = "Usage: #{File.basename($0)} [options]"
         opts.separator ''
-        opts.on('-s', '--server', 'Run as a web server') { conf.mode = :server }
-        opts.on('-p', '--port PORT', "Port for web server (defaults to #{conf.port})") { |port| conf.port = port }
+        opts.on('-s', '--server', 'Run as a web server') { @conf.mode = :server }
+        opts.on('-p', '--port PORT', "Port for web server (defaults to #{@conf.port})") { |port| @conf.port = port }
         opts.on('-h', '--help', 'Show this message') { puts opts; exit }
         opts.separator ''
       end
 
-      opts.parse! args
+      opts.parse! argv
+    end
 
-      case conf.mode
+    def run!
+      case @conf.mode
       when :console
         Perquackey::Console.run!
       when :server
-        Perquackey::Server.run!(:port => conf.port)
+        Perquackey::Server.run!(:port => @conf.port)
       end
     end
   end

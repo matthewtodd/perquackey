397f1a4527fedfd751f0cc59e74283df63eb2ae2
Matthew Todd <matthew.todd@gmail.com>
2010-04-13 09:01:12 +0300

    Be a little more overflow-safe in string ext.

--- ext/perquackey/string_extensions.c
+++ ext/perquackey/string_extensions.c
@@ -1,26 +1,22 @@
 #include "ruby.h"
 
-#define MAX_LENGTH 40
-#define XOUT '@'
-
 static VALUE mPerquackey;
 static VALUE mStringExtensions;
 
 VALUE StringExtensions_spell_p(VALUE self, VALUE other) {
-  char letter_set[MAX_LENGTH];
-  register char *letter;
+  VALUE scratch_pad_value;
+  register char *scratch_pad;
+  register char *scratch_pad_ptr;
+  register char *other_ptr;
 
-  char *word;
-  char *letters;
+  scratch_pad_value = rb_str_dup(self);
+  rb_str_modify(scratch_pad_value);
+  scratch_pad = RSTRING(scratch_pad_value)->ptr;
+  other_ptr = RSTRING(other)->ptr;
 
-  letters = RSTRING(self)->ptr;
-  word = RSTRING(other)->ptr;
-
-  strcpy(letter_set, letters);
-
-  while (*word) {
-    if ((letter = strchr(letter_set, *word++)) != NULL) {
-      *letter = XOUT;
+  while (*other_ptr) {
+    if ((scratch_pad_ptr = strchr(scratch_pad, *other_ptr++)) != NULL) {
+      *scratch_pad_ptr = '@';
     } else {
       return Qfalse;
     }

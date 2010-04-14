454252dd55834e19f8c20d12e72d976a1ef8d080
Matthew Todd <matthew.todd@gmail.com>
2010-04-14 12:17:26 +0300

    Test readability.

--- test/word_table_test.rb
+++ test/word_table_test.rb
@@ -2,16 +2,22 @@ require 'test/helper'
 
 class WordTableTest < Test::Unit::TestCase
   def test_handles_empty_list
-    empty_table = Perquackey::WordTable.new []
+    empty_table = Perquackey::WordTable.new %w()
 
     assert_equal [], empty_table.headers.to_a
-    assert_equal [], empty_table.collect { |row| row }
+    assert_equal [], empty_table.to_a
   end
 
   def test_handles_some_list
-    table = Perquackey::WordTable.new %w[ape bear cat dog eagle fox goose hippo ibis]
+    table = Perquackey::WordTable.new %w(ape bear cat dog eagle fox goose hippo ibis)
 
     assert_equal [3, 4, 5], table.headers.to_a
-    assert_equal ['ape bear eagle', 'cat ibis goose', 'dog      hippo', 'fox           '], table.collect { |row| row.join(' ') }
+
+    assert_equal [
+      'ape bear eagle',
+      'cat ibis goose',
+      'dog      hippo',
+      'fox           '
+    ], table.collect { |row| row.join(' ') }
   end
 end

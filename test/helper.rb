6942fa5817b261a65d6720cbd11644e794b2cf25
Matthew Todd <matthew.todd@gmail.com>
2010-04-21 12:11:22 +0300

    `gem check --test perquackey` should Just Work.

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,9 @@
-require 'perquackey'
 require 'test/unit'
+require 'perquackey'
 
-if $stdout.tty?
-  require 'redgreen'
+begin
+  require 'redgreen' if $stdout.tty?
+rescue LoadError
+  # It's nice not to have hard dependencies on any gems for testing, so that
+  # it's super-easy for your users to run `gem check --test perquackey.`
 end
